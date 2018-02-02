b54cd509007b89ec81872fb9d767f49aeb972cdd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-02-02 17:54:42 +0700

    Hello world with styled-components and reset.css

--- web/index.js
+++ web/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { injectGlobal } from "styled-components";
 import App from "./src/App";
+import reset from "styled-reset";
+
+injectGlobal`
+${reset}
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
