6535de1c12e99dca3ad0f8cd8c9ad1d18c861109
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-13 15:40:53 +0900

    03-Animations

---
+++ 03-animations/index.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from "react";
+import styled, { injectGlobal, keyframes } from "styled-components";
+/*
+    Goal:
+        - Make a button
+        - Give hover and active states
+        - Change the background color of the button depending on the props
+*/
+
+const Button = styled.button`
+  -webkit-appearance: none;
+  border-radius: 50px;
+  padding: 5px 10px;
+  min-width: 200px;
+  color: white;
+  border: 0;
+  font-weight: 600;
+  cursor: pointer;
+  background-color: ${props => (props.danger ? "#e74c3c" : "#57b65f")};
+  font-size: 14px;
+  &:focus,
+  &:active {
+    outline: none;
+  }
+  &:hover {
+    background-color: ${props => (props.danger ? "#f05d4c" : "#65d36e")};
+  }
+`;
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+}
+
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const RotatingBtn = Button.extend`
+  ${props => {
+    if (!props.danger) {
+      return `animation: ${rotation} 2s linear infinite;`;
+    }
+  }};
+`;
+
+const App = () => (
+  <Fragment>
+    <RotatingBtn>Button!</RotatingBtn>
+    <RotatingBtn danger>Button!</RotatingBtn>
+  </Fragment>
+);
+
+injectGlobal`
+  body{
+    background-color:pink;
+    font-family:Helvetica;
+  }
+`;
+
+export default App;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./03-injectAndExtend";
+import App from "./03-animations";
 
 ReactDOM.render(<App />, document.getElementById("react"));
