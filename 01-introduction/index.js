cfd7e87b262f5416c94c2c1ca62741646d4b2102
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-13 14:45:34 +0900

    Project Setup

---
+++ 01-introduction/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <h1>Welcome to Styled</h1>;
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./01-introduction";
+
+ReactDOM.render(<App />, document.getElementById("react"));
